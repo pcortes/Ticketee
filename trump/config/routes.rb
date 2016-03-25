c24d700375720bb7c25d225b97599de003b988c0
Philip Cortes <philip.cortes@gmail.com>
2016-03-25 10:07:23 -0700

    started over with pg as original db, then created trump user in postgres, and now bundle install works, as do database migrations.

--- trump/config/routes.rb
+++ trump/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-    root "projects#index"
 
-    resources :projects
+
+   get 'welcome/index'
+  root 'welcome#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
